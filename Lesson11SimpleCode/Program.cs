247935cae98a4769ecdd02146d9af1a8afdebcb0
Sergey Evseev <sevseev@gmail.com>
2022-09-19 01:55:47 +0300

    1st triangle

--- Lesson11SimpleCode/Program.cs
+++ Lesson11SimpleCode/Program.cs
@@ -7,14 +7,26 @@ namespace LessonsSimpleCode
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 3; i++)
+            Console.WriteLine("Введите высоту треугольника");
+            int height = int.Parse(Console.ReadLine());                     
+
+
+            for (int i = 0; i <= height; i++) //перебор по строкам
             {
-                Console.WriteLine("Цикл 1 итерация №: " + i);
-                for (int j = 0; j < 5; j++)
-                {
-                    Console.WriteLine("\tЦикл 2 итерация №: " + j);
-                }
+               Console.WriteLine(); //каждую строку начинаем с начала
+                
+                for (int j = 0; j < i; j++)
+                { //в 1-й строке печатаем 1 звездочку, в последней i звездочек
+                    Console.Write("* ");
+                }                
             }
+            Console.WriteLine();
         }        
     }
 }
+
+//for (int k = width; k > i; k--)
+
+//{
+//    Console.Write(" ");
+//}
