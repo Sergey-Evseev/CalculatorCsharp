a5943a54c92b34f63ae44ebf626867b1595f5882
Sergey Evseev <sevseev@gmail.com>
2022-09-18 17:04:59 +0300

    GamesInfo deleted

--- Lesson11SimpleCode/Program.cs
+++ Lesson11SimpleCode/Program.cs
@@ -5,25 +5,6 @@ namespace LessonsSimpleCode
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            var gamesInfo = GetGamesInfo(); //метод возвращает список объектов класса GamesInfo
 
-            for (int i = 0; i < gamesInfo.Count; i++)
-            {
-                Console.WriteLine($"{gamesInfo[i].TypeOfGame}:");
-                for (int j = 0; j < gamesInfo.Games.Count; j++)
-                {
-                    Console.WriteLine($"{gamesInfo[i].Name[j].Name}");
-                }
-                Console.WriteLine();
-            }
-            Console.ReadLine();
-        }
-        new GameInfo()
-        {
-            TypeOfGame = GameType.Shooter;
-        }
-        
-    }         
-}    
+    }
+}
