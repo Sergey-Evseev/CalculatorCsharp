3903a458ca46c6b3601adc38941a44b30c44a71a
Sergey Evseev <sevseev@gmail.com>
2022-09-18 17:02:42 +0300

    GamesInfo

--- Lesson11SimpleCode/Program.cs
+++ Lesson11SimpleCode/Program.cs
@@ -5,33 +5,24 @@ namespace LessonsSimpleCode
 {
     class Program
     {
-        static int[,] GetRandomArray(int rows, int columns)
-        {
-            Random random = new Random();
-            var result = new int[rows, columns]; //создаем новый массив интов и присваиваем его переменной
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < columns; j++)
-                {
-                    result[i, j] = random.Next(100);//заполнение массива
-                }
-            }
-            return result;
-        }
-
         static void Main(string[] args)
         {
-            int rows = 5;
-            int columns = 10;
-            var myArray = GetRandomArray(rows, columns);
+            var gamesInfo = GetGamesInfo(); //метод возвращает список объектов класса GamesInfo
 
-            for (int i = 0; i < rows; i++)
+            for (int i = 0; i < gamesInfo.Count; i++)
             {
-                for (int j = 0; j < columns; j++)
+                Console.WriteLine($"{gamesInfo[i].TypeOfGame}:");
+                for (int j = 0; j < gamesInfo.Games.Count; j++)
                 {
-                    Console.Write($"{myArray[i,j]}\t");
+                    Console.WriteLine($"{gamesInfo[i].Name[j].Name}");
                 }
+                Console.WriteLine();
             }
+            Console.ReadLine();
+        }
+        new GameInfo()
+        {
+            TypeOfGame = GameType.Shooter;
         }
         
     }         
