3d2d8850183148d0eea084ac045287c8c1b4cf6a
Sergey Evseev <sevseev@gmail.com>
2022-09-19 04:03:44 +0300

    Master commit

--- Lesson11SimpleCode/Program.cs
+++ Lesson11SimpleCode/Program.cs
@@ -5,6 +5,16 @@ namespace LessonsSimpleCode
 {
     class Program
     {
-
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine("Цикл 1 итерация №: " + i);
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.WriteLine("\tЦикл 2 итерация №: " + j);
+                }
+            }
+        }        
     }
 }
