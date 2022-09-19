d1333e06933b1edb83309c608293e3ce07b49f63
Sergey Evseev <sevseev@gmail.com>
2022-09-19 05:29:51 +0300

    Array Initialising

--- Lesson11SimpleCode/Program.cs
+++ Lesson11SimpleCode/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+
 
 namespace LessonsSimpleCode
 {
@@ -7,26 +9,27 @@ namespace LessonsSimpleCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите высоту треугольника");
-            int height = int.Parse(Console.ReadLine());                     
-            //введенный параметр опеределяет количество строк
+            int [] myArray = new int[5]; // выделение памяти сразу с момент обявления массива
 
-            for (int i = 0; i <= height; i++) //перебор по строкам
+            int [] myArray1 = new int[5] {1, 2, 10, 15, 6 }; // инициализация в момент объявления и выделения памяти
+
+            int[] myArray2 = new int[] { 100, 3, 27, 600 }; // выделение памяти в момент инициализации
+
+            int[] myArray3 = { 100, 3, 27, 600 }; // выделение памяти в момент инициализации
+                        
+
+            int[] myArray4 = Enumerable.Repeat(5, 10).ToArray();
+            //Repeat<TResult>(TResult, Int32)	
+            // Генерирует последовательность, содержащую одно повторяющееся значение.
+
+            int[] myArray5 = Enumerable.Range(98, 15).ToArray();
+
+
+            foreach (var i in myArray5)
             {
-               Console.WriteLine(); //каждую строку начинаем с начала
-                               
-                for (int k = height; k > i; k--)
-                { 
-                    Console.Write(" ");
-                }
-
-                for (int j = 0; j < i; j++)
-                { 
-                    Console.Write("* ");
-                }
-
+                Console.Write("{0} ", i);
             }
-            Console.WriteLine();
+            Console.WriteLine();            
         }        
     }
 }
